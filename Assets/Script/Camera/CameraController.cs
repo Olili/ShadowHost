5676d040299e43458e9b9ea60790174a30b98e0c
ZephyrRaine <zephyr.raine@gmail.com>
2017-11-22 11:43:25 +0100

    Camera Fix + lights

--- Assets/Script/Camera/CameraController.cs
+++ Assets/Script/Camera/CameraController.cs
@@ -5,16 +5,24 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour {
 
+    private CinemachineTransposer transposer;
     private CinemachineVirtualCamera cineVC;
-
+    public float FollowOffsetForward;
     public void Awake()
     {
         cineVC = GetComponentInChildren<CinemachineVirtualCamera>();
+        transposer = cineVC.GetComponent<CinemachineTransposer>();
+    }
+    
+    void Update()
+    {
+    //    transposer.m_FollowOffset = transform.forward * FollowOffsetForward;
     }
 
-    public void SetTarget(Transform transform)
+    public void SetTarget(Transform _transform)
     {
-        cineVC.Follow = GameManager.Instance.PlayerBrain.transform;
+        cineVC.Follow = (_transform);
     }
+
   
 }
